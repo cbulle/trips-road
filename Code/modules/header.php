64bb2ded35640ea084763088f4bdec5075666626
iutbgdin <iutbgdin@iutabgdinvm-146.iutbourg.univ-lyon1.fr>
2025-11-07 11:53:33 +0100

    arrangement header

--- Code/modules/header.php
+++ Code/modules/header.php
@@ -3,50 +3,55 @@
 <head>
   <meta charset="UTF-8">
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="style.css"> 
 </head>
 <body>
   
-</body>
-
-    <header>
+  <header>
     <nav>
-        
-
-    <ul>
-      <i class="material-icons">search</i>
-      <i class="material-icons">settings_accessibility</i>
-      <li><a href="/index.php" >Trips & Roads</a></li>
-      <i class="material-icons">chat_bubble</i>
-      <li><a href="" >Messagerie</a></li>
-      <i class="material-icons">group</i>
-      <li><a href="" >Amis</a></li>
-      <i class="material-icons">add_box</i>
-      <li><a href="/creerRoad.php" >Créer un Road-Trip</a></li>
-      <li><a href="/profil.php" ><img src="../img/User.png" alt="Photo de profil" ></a></li>
+      <ul>
+        <div class="nav-item">
+          <a href="/index.php"><i class="material-icons">search</i></a>
+        </div>
+        <div class="nav-item">
+          <a href="/index.php"><i class="material-icons">settings_accessibility</i></a>
+        </div>
+        <div class="title">
+          <li><a href="/index.php">Trips & Roads</a></li>
+        </div>
+        <div class="nav-item">
+          <a href="/index.php"><i class="material-icons">chat_bubble</i>
+          <span>Messagerie</span> </a>
+        </div>
+        <div class="nav-item">
+          <a href="/index.php"><i class="material-icons">group</i>
+          <span>Amis</span> </a>
+        </div>
+        <div class="nav-item">
+          <a href="/creationRoadTrip.php"><i class="material-icons">add_box</i>
+          <span>Créer un Road-Trip</span> </a>
+        </div>
+        <div class="nav-item">
+          <a href="/index.php"><i class="material-icons">account_circle</i>
+          <span>Se connecter</span> </a>
+        </div>
+      </ul>
       
-    </ul>
-      
-      
-      
-      
-    
-      
-     <input type="checkbox" id="burger">
-    <label for="burger" class="burger">
-      <span> </span>
-
-</label>
-        <ul class = "ul_burger">
-          <li><a href="" >Mes Roads-Trips</a> </li>
-          <li><a href="" >Historique</a></li>
-          <li><a href="" >Favoris</a></li>
-          <li><a href="" >Paramètre de compte</a></li>
-          <li><a href="" >Aide / FAQ</a></li>
-          <li><a href="" >A propos / Contact</a></li>
-          <li><a href="" >Déconnexion</a></li>
-        </ul>
-      
+      <input type="checkbox" id="burger">
+      <label for="burger" class="burger">
+        <span> </span>
+      </label>
+      <ul class="ul_burger">
+        <li><a href="#">Mes Roads-Trips</a></li>
+        <li><a href="#">Historique</a></li>
+        <li><a href="#">Favoris</a></li>
+        <li><a href="#">Paramètre de compte</a></li>
+        <li><a href="#">Aide / FAQ</a></li>
+        <li><a href="#">A propos / Contact</a></li>
+        <li><a href="#">Déconnexion</a></li>
+      </ul>
     </nav>
-    </header>
-
+  </header>
+  
+</body>
 </html>
