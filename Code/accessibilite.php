e9bfb2a5977158c7af0d6b00e565ed8808983273
BULLE CELESTE p2301500 <celeste.bulle@etu.univ-lyon1.fr>
2025-11-21 15:59:42 +0000

    Add new file

---
+++ Code/accessibilite.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href= "/css/style.css">
+    <title>Accessibilité</title>
+</head>
+<body>
+    <?php include __DIR__ . "/modules/header.php" ?>
+    <div class="cont_access">
+        <form id="AccessForm" class="AccessForm" action = " " method = "post">
+        <h2 id="login-title">Accessibilité </h2>
+        <label for="btnSombre" id="btnLabel">Mode sombre :</label>
+        <div class="btnSombre">
+        <label class="switch" for="checkboxSombre">
+            <input type="checkbox" id="checkboxSombre" />
+            <div class="slider round"></div>
+            
+        </label>
+</div>
+<script src="js/map.js"></script>  
+
+
+    <?php include __DIR__ . "/modules/footer.php" ?>
+</body>
+</html>
