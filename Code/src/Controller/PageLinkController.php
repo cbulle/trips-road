83809dfe998ab605be7b462b036c32241e2ba8ce
p2405908 <adrien.bayssat@etu.univ-lyon1.fr>
2026-02-04 16:07:51 +0100

    page link fait

---
+++ Code/src/Controller/PageLinkController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class PageLinkController extends AppController
+{
+
+    public function contact(){
+
+    }
+
+    public function faq(){
+
+    }
+
+    public function cgu(){
+
+    }
+
+    public function road_trip(){
+
+    }
+
+    public function cookie(){
+
+    }
+
+    public function politique(){
+
+    }
+}
