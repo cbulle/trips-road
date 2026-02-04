04fed092137a85ae3810a0c3c33ab6697eb09cdf
Sasha Lambert <sasha.lambert@etu.univ-lyon1.fr>
2026-02-04 10:20:28 +0100

    Opti vu modal

---
+++ Code/src/Controller/PageLinkController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class PageLinkController extends AppController
+{
+    public function cgu(){
+
+    }
+    public function contact(){
+
+    }
+    public function faq(){}
+    public function confidentiality(){}
+
+    public function cookies(){}
+
+    public function roadTrips(){}
+
+}
